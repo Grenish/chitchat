dcdc0cc3ce909a9e567776fd71610c0b085b5b9e
Grenish rai <ariestheracer@gmail.com>
2024-02-29 23:03:51 +0530

    Add room generation endpoint and socket event handlers

--- server/index.js
+++ server/index.js
@@ -1,16 +1,45 @@
 import express from "express";
 import cors from "cors";
 import { Server } from "socket.io";
+import http from "http";
+import { v4 as uuidv4 } from 'uuid';
+
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   res.send("Hello World!");
+});
+
+// Add this endpoint to generate a new room ID
+app.post("/rooms", (req, res) => {
+  const roomId = uuidv4();
+
+  res.json({ roomId });
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
