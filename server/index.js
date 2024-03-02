0850d955287cdca2daa27cfb189381a4542998c5
Grenish rai <ariestheracer@gmail.com>
2024-03-02 19:50:33 +0530

    Add POST endpoint for creating rooms

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ import express from "express";
 import cors from "cors";
 import { Server } from "socket.io";
 import http from "http";
-import { v4 as uuidv4 } from 'uuid';
-
+import { v4 as uuidv4 } from "uuid";
 
 const app = express();
 app.use(cors());
@@ -25,6 +24,20 @@ app.get("/", (_, res) => {
   res.send("Hello World!");
 });
 
+app.post("/rooms", (req, res) => {
+  const { username } = req.body;
+
+  // Generate a new room ID
+  const roomId =
+    uuidv4().split("-")[1] +
+    "-" +
+    Math.floor(Math.random() * 1000) +
+    "-" +
+    uuidv4().split("-")[0];
+
+  // Respond with the room ID
+  res.json({ roomId });
+});
 
 io.on("connection", (socket) => {
   console.log("a user connected");
